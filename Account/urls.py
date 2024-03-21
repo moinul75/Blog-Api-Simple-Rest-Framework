aa679cca26973a645072d349f9dc98b0120e95bd
moinul75 <freelancerudoy752@gmail.com>
2024-03-21 22:05:07 +0600

    added profile urls

--- Account/urls.py
+++ Account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import RegistrationApi,LoginApi
+from .views import RegistrationApi,LoginApi,ProfileListCreateAPIView, ProfileRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('register',RegistrationApi.as_view()),
-    path('login',LoginApi.as_view()),
+    path('login',LoginApi.as_view()), 
+    path('profiles/', ProfileListCreateAPIView.as_view(), name='profile-list-create'),
+    path('profiles/<int:pk>/', ProfileRetrieveUpdateDestroyAPIView.as_view(), name='profile-detail'),
 ]
